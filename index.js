e747ed06f4b719ffcd66d3c21a40eab159543ce6
Chimwemwe Masona <justplainrodney@gmail.com>
2023-08-12 00:30:56 +0200

    update

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-const { mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 const User = require("./models/user");
+const Post = require("./models/post");
 const bcrypt = require("bcryptjs");
 const app = express();
 const jwt = require("jsonwebtoken");
 const cookieParser = require('cookie-parser')
+const multer  = require('multer')
+const uploadMiddleware = multer({ dest: 'uploads/' })
+const fs = require('fs');
+const path = require("path");
 
 const salt = bcrypt.genSaltSync(10);
 const secret = "clna;sdkfmadlkfwmera";
@@ -16,7 +21,12 @@ app.use(cookieParser());
 
 mongoose.connect(
   "mongodb+srv://justplainrodney:YURZPRiMet5Dj22@cluster0.0yjtior.mongodb.net/?retryWrites=true&w=majority"
+  // password: YURZPRiMet5Dj22
 );
+
+app.get("/test", (req, res) => {
+  res.send("Hello World");
+});
 
 app.post("/register", async (req, res) => {
   const { firstName, lastName, email, password, passwordConfirmation } =
@@ -67,6 +77,50 @@ app.get('/profile', (req, res) => {
 
 app.post('/logout', (req, res) => {
   res.cookie('token', '').json('ok')
+})
+
+app.use('/uploads', express.static('uploads'));
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
+  const {originalname,path} = req.file;
+  const parts = originalname.split('.')
+  const ext = parts[parts.length - 1]
+  const newPath = path+'.'+ext
+  fs.renameSync(path, newPath) 
+
+  const {token} = req.cookies;
+
+  jwt.verify(token, secret, {}, async(err, info) => {
+    if (err) {
+      throw err
+    }
+
+    const {title, summary, content} = req.body;
+    const postDoc = await Post.create({
+     title,
+       summary,
+       content,
+       cover:newPath,
+       author:info.id,
+    });
+    res.json(postDoc)
+
+   
+  })
+})
+
+
+app.get("/posts", async (req, res) => {
+  res.json(await Post.find()
+  .populate('author', ['username'])
+  .sort({createdAt: -1})
+  .limit(20)
+  );
+});
+
+app.get('/posts/:id', async (req, res) => {
+  const { id } = req.params;
+ const postDoc = await Post.findById(id).populate('author', ['username'])
+ res.json(postDoc);
 })
 
 app.listen(4000);
